cd3377a685c7a15946bface536a1a5b0b0449be9
VasiliyPolyakov <polyakovvn92@gmail>
2016-12-27 14:47:22 +0200

    united all the files js

--- works/Web_page-construction-company/gulpfile.js
+++ works/Web_page-construction-company/gulpfile.js
@@ -3,6 +3,8 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var gutil = require('gulp-util');
 
 gulp.task('styles', function() {
 	return gulp.src('src/styles/**/*.less')
@@ -11,7 +13,14 @@ gulp.task('styles', function() {
 		.pipe(gulp.dest('dist'));
 });
 
-
 gulp.task('watch:styles', function() {
 	gulp.watch(['src/styles/**/*.less'], ['styles']);
+});
+
+gulp.task('scripts', function() {
+	return gulp.src('src/js/**/*.js')
+		.pipe(concat('main.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'))
+		.on('error', gutil.log)
 });
