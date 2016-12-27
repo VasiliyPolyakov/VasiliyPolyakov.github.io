dc3f675f0cbedd744e4db5f7f2252d795d0f66b5
VasiliyPolyakov <polyakovvn92@gmail>
2016-12-27 13:46:13 +0200

    united all the less files

--- works/Web_page-construction-company/gulpfile.js
+++ works/Web_page-construction-company/gulpfile.js
@@ -1,15 +1,12 @@
 'use strict';
 
 var gulp = require('gulp');
-var stylus = require('gulp-less');
+var less = require('gulp-less');
 var concat = require('gulp-concat');
 
 gulp.task('styles', function() {
 	return gulp.src('src/styles/**/*.less')
 		.pipe(less())
-		.pipe(postcss([autoprefixer({
-			browsers: ['last 2 versions']
-		})]))
 		.pipe(concat('all.css'))
 		.pipe(gulp.dest('dist'));
 });
